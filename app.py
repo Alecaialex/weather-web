7906f45b6e808eb02dd0687821d9e34040d28635
Alejandro Sanz <46007072+Alecaialex@users.noreply.github.com>
2023-11-17 21:06:38 +0100

    Update app.py

--- app.py
+++ app.py
@@ -10,11 +10,10 @@ def index():
 @app.route('/weather', methods=['POST'])
 def get_weather():
     city = request.form['city']
-    api_key = '7b3a3aacea0a6f77258ad667236054a9'
-    weather_url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}'
+    url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid=7b3a3aacea0a6f77258ad667236054a9'
     
-    response = requests.get(weather_url)
-    weather_data = response.json()
+    r = requests.get(url)
+    weather_data = r.json()
 
     temperature = weather_data['main']['temp']
     description = weather_data['weather'][0]['description']
