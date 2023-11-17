4466989c35b9bde892fd6f9b762265afb7e2259a
Alejandro Sanz <46007072+Alecaialex@users.noreply.github.com>
2023-11-17 21:11:32 +0100

    data

--- app.py
+++ app.py
@@ -10,15 +10,18 @@ def index():
 @app.route('/weather', methods=['POST'])
 def get_weather():
     city = request.form['city']
-    url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid=7b3a3aacea0a6f77258ad667236054a9'
+    url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&lan=es&appid=7b3a3aacea0a6f77258ad667236054a9'
     
     r = requests.get(url)
-    weather_data = r.json()
+    data = r.json()
 
-    temperature = weather_data['main']['temp']
-    description = weather_data['weather'][0]['description']
+    humidity = data['main']['humidity']
+    pressure = data['main']['pressure']
+    wind = data['wind']['speed']
+    description = data['weather'][0]['description']
+    temp = data['main']['temp']
 
-    return render_template('index.html', city=city, temperature=temperature, description=description)
+    return render_template('index.html', city=city, temperature=temp, description=description, wind=wind, humidity=humidity, pressure=pressure)
 
 if __name__ == '__main__':
     app.run(debug=True)
