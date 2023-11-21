9cb8ae802ce2ac9652cbd0fa22619af95e57622a
Alejandro Sanz <46007072+Alecaialex@users.noreply.github.com>
2023-11-21 20:28:35 +0100

    setup key arreglada y fondo de weather arreglado

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ os.system("pip install -r requirements.txt")
 
 api_key = str(input("Please, insert your API key: "))
 with open("config.py", "w") as config_file:
-    config_file.write(f"API_KEY ='{api_key}'")
+    config_file.write(f"API_KEY = '{api_key}'")
 
 print(f"\n\n\nFrom now on execute the app.py file unless you want to change API key...\n")
 os.system("python app.py")
