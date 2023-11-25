ec2064435f6118e9ced0e899916db0c4ef6376da
Alejandro Sanz <46007072+Alecaialex@users.noreply.github.com>
2023-11-25 12:11:55 +0100

    js update
    quitado javascript innecesario y que no funcionaba de las animaciones del sol y la nube

--- static/js/script.js
+++ static/js/script.js
@@ -1,21 +1,3 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const form = document.querySelector('.imagen_nube');
-    const images = document.querySelectorAll('.imagen_nube');
-
-    form.addEventListener('mouseenter', function () {
-        images.forEach(image => {
-            image.style.animation = 'float 0.5s ease-in-out infinite';
-        });
-    });
-
-    form.addEventListener('mouseleave', function () {
-        images.forEach(image => {
-            image.style.animation = '';
-        });
-    });
-});
-
-
 //Enviar el formulario al pulsar enter
 document.getElementById('weatherForm').addEventListener('keydown', function (event) {
     if (event.key === 'Enter') {
